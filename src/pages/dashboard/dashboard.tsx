e0dec27ad7728df6d8d76ed4e35da10cab3e1f69
gabriel <gabrielfmoreira1@gmail.com>
2025-12-18 07:21:28 -0300

    secund commit

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
 import { Sidebar } from "../../components/sidebar/sibebar";
 import logo1 from "../../assets/Logo-1 1.png";
 import "./dashboard.css";
 import construcion from "../../assets/construcion.svg";
 
 export function Dashboard() {
-  const [openMenu, setOpenMenu] = useState<string | null>(null);
-
-  function toggleMenu(label: string) {
-    setOpenMenu(openMenu === label ? null : label);
-  }
-
   return (
     <div className="dashboard-container">
       <Sidebar />
@@ -18,6 +11,7 @@ export function Dashboard() {
       <div className="dashboard-content">
         <img src={logo1} alt="Maestro" className="logo-icon" />
         <h1>Maestro</h1>
+
         <div className="dashboard-info">
           <img
             src={construcion}
